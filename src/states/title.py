ded86c0da93a512ff141e9905f0b83bd09b8e937
jamesin4d <jdiel9674@gmail.com>
2015-11-01 05:23:27 -0600

    commit to it!

--- src/states/title.py
+++ src/states/title.py
@@ -7,17 +7,37 @@
 # --------------------------------------------------------------------
 from ..eng import State
 from game import Game
-from ..utilities import image_handling
+from ..utilities import image_handling, graphicaluserinterface
 import pygame
 
 
 class Title(State):
     def __init__(self):
         State.__init__(self)
+
         self.canvas = image_handling.get_image('img/titlescreen/title.png')
         self.next_state = Game()
         self.canvas = pygame.transform.scale(self.canvas, self.screen.get_size())
-        self.img_pos = image_handling.center(self.canvas.get_size(), self.screen.get_size())
+        self.canvas_pos = image_handling.center(self.canvas.get_size(), self.screen.get_size())
+        self.set_up_user_interface()
+
+
+    def set_up_user_interface(self):
+        logo = image_handling.get_image('img/titlescreen/titlelogo.png')
+        logo_pos = (300,10)
+        self.canvas.blit(logo,logo_pos)
+        load = image_handling.get_image('img/titlescreen/load.png')
+        load_pos = (650,315)
+        self.canvas.blit(load,load_pos)
+        new = image_handling.get_image('img/titlescreen/new.png')
+        new_pos = (650,270)
+        self.canvas.blit(new,new_pos)
+        option = image_handling.get_image('img/titlescreen/option.png')
+        option_pos = (650,360)
+        self.canvas.blit(option,option_pos)
+        quit = image_handling.get_image('img/titlescreen/quit.png')
+        quit_pos = (650,405)
+        self.canvas.blit(quit,quit_pos)
 
 
 
@@ -32,7 +52,7 @@ class Title(State):
 
     def update_screen(self):
         self.screen.fill((0,0,0))
-        self.screen.blit(self.canvas, self.img_pos)
+        self.screen.blit(self.canvas, self.canvas_pos)
         pygame.display.update()
 
     def quit(self):
